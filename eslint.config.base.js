8961bea529d080dc8fd431b1f0c9d1e20771fbea
Yestay <yestayyessenbay@gmail.com>
2025-05-29 01:35:05 +0900

    chore(client server): Update folder structure

---
+++ eslint.config.base.js
@@ -0,0 +1,25 @@
+import js from '@eslint/js';
+import typescript from '@typescript-eslint/eslint-plugin';
+import typescriptParser from '@typescript-eslint/parser';
+import prettier from 'eslint-config-prettier';
+
+export default [
+  js.configs.recommended,
+  {
+    files: ['**/*.ts', '**/*.tsx'],
+    plugins: {
+      '@typescript-eslint': typescript,
+    },
+    languageOptions: {
+      parser: typescriptParser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+      },
+    },
+    rules: {
+      ...typescript.configs['recommended'].rules,
+    },
+  },
+  prettier,
+];
